ce7fbf4b41559bb48efd92f0d484da4b79d960b7
nerd hnd <hndpp@nerdspc>
2022-10-19 18:32:28 +0800

    ADD(lazy_for): set up LazyFor componemt
    -...
    
    DIRECT COMMIT

---
+++ page-serve/web/src/component/lazy_for.rs
@@ -0,0 +1,15 @@
+use yew::prelude::*;
+
+pub(crate) struct Props {
+    item: Children,
+    padding: Option<Children>,
+}
+
+#[function_component(LazyFor)]
+pub(crate) fn lazy_for() -> Html {
+    html! {
+        <>
+
+        </>
+    }
+}
---
+++ page-serve/web/src/component/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod main_header;
+pub(crate) mod lazy_for;
